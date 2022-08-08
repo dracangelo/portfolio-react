87437314e1268dee09a4fe70bea89bb52b540ee2
dracangelo <kelvinmbuguaw@gmail.com>
2022-08-08 10:33:43 +0300

    awsome layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
@@ -7,7 +8,12 @@ const Layout = () => {
             <Sidebar />
 
             <div className="page">
-                <span
+                <span className="tags top-tags"> &lt;body&gt;</span>
+                <Outlet />
+                <span className="tags bottom-tags"> &lt;body&gt;
+                <br />
+                <span className="bottom-tag-html"> &lt;/html&gt;</span>
+                </span>
             </div>
         </div>
     )
